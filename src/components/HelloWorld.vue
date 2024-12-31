990e7eab94eba1773679242a0604dcf0cb95670e
Murtaki <murtakibangko@gmail.com>
2024-12-31 10:22:32 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+    Selamat Belajar VUe
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
